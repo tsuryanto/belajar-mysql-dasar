ff8682825b6057645df5ea1a2e2f4bc5689c6df8
tsuryanto <tsuryanto16@gmail.com>
2022-05-23 20:46:24 +0700

    Locking

--- user1.sql
+++ user1.sql
@@ -9,3 +9,39 @@ SET title = 'Diubah oleh User 1'
 WHERE id = 9;
 
 COMMIT;
+
+#  DEADLOCK
+
+START TRANSACTION ;
+
+SELECT * FROM products WHERE id = 'P0001' FOR UPDATE ;
+
+SELECT * FROM products WHERE id = 'P0002' FOR UPDATE ;
+
+# LOCK TABLE READ
+
+LOCK TABLES products READ;
+
+UPDATE products
+SET quantity = 100
+WHERE id = 'P0001';
+
+UNLOCK TABLES ;
+
+# LOCK TABLE WRITE
+
+LOCK TABLES products WRITE ;
+
+UPDATE products
+SET quantity = 100
+WHERE id = 'P0001';
+
+SELECT * FROM products;
+
+UNLOCK TABLES ;
+
+# LOCK INSTANCE
+
+LOCK INSTANCE FOR BACKUP;
+
+UNLOCK INSTANCE;
--- user2.sql
+++ user2.sql
@@ -19,3 +19,28 @@ SET quantity = quantity - 10
 WHERE id = 'P0001';
 
 COMMIT;
+
+#  DEADLOCK
+
+START TRANSACTION ;
+
+SELECT * FROM products WHERE id = 'P0002' FOR UPDATE ;
+
+SELECT * FROM products WHERE id = 'P0001' FOR UPDATE ;
+
+# LOCK TABLE READ
+
+SELECT * FROM products;
+
+UPDATE products
+SET quantity = 100
+WHERE id = 'P0001';
+
+# LOCK TABLE WRITE
+
+SELECT * FROM products;
+
+# LOCK INSTANCE
+
+ALTER TABLE products
+ADD column sample VARCHAR(100);
